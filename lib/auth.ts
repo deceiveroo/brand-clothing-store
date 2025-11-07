f124a729fb4d5ab39053fa06f15395d43d0f520f
deceiveroo <ahterkin1@mail.ru>
2025-11-07 17:47:51 +0300

    feat: add enhanced notification system with gradients and animations

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from '@/lib/prisma';
-import bcrypt from "bcryptjs";
+import { bcrypt } from "bcryptjs";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
